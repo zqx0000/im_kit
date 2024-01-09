74d0759241ed5caabcfa941da65c920b0fe9572d
赵杰（Android） <zhaojie0532vip@126.com>
2024-01-09 14:30:58 +0800

    1.0.11

--- app/src/main/java/com/yimaxiaoerlang/im_kit/AppApplication.java
+++ app/src/main/java/com/yimaxiaoerlang/im_kit/AppApplication.java
@@ -10,8 +10,8 @@ public class AppApplication extends Application {
         super.onCreate();
 
         YMIMKit.getInstance().setAppContext(this);
-        YMIMKit.getInstance().initIM("o5728KP83S097fE60vTz", "S7D56527914yodN9zO9e");
-        YMIMKit.getInstance().initRTM("o5728KP83S097fE60vTz", "S7D56527914yodN9zO9e");
+        YMIMKit.getInstance().initIM("09r277Y47OA8Bd7P69e3", "X5Tm26F1D95759yEvoA5");
+        YMIMKit.getInstance().initRTM("09r277Y47OA8Bd7P69e3", "X5Tm26F1D95759yEvoA5");
 //        YMIMKit.getInstance().configAddress("https://im.yimaxiaoerlang.cn", "wss://im.yimaxiaoerlang.cn/websocket", "wss://im.yimaxiaoerlang.cn/signal");
         YMIMKit.getInstance().configAddress("http://192.168.124.33:8001", "ws://192.168.124.33:8001/websocket", "ws://192.168.124.33:8001/signal");
     }
--- im_kit/src/main/java/com/yimaxiaoerlang/im_kit/utils/PermissionsCommon.java
+++ im_kit/src/main/java/com/yimaxiaoerlang/im_kit/utils/PermissionsCommon.java
@@ -1,10 +1,6 @@
 package com.yimaxiaoerlang.im_kit.utils;
 
-import android.Manifest;
-
 import androidx.fragment.app.FragmentActivity;
-
-import pub.devrel.easypermissions.EasyPermissions;
 
 
 public class PermissionsCommon {
@@ -15,49 +11,16 @@ public class PermissionsCommon {
     }
 
     public void requestCAMERA(PermissionsCallback callback) {
-        String[] perms = {Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE};
-
-        if (EasyPermissions.hasPermissions(fragmentActivity, perms)) {
-            callback.onSuccess();
-        } else {
-
-            EasyPermissions.requestPermissions(fragmentActivity, "请给予录音、相机、存储权限",
-                    1, perms);
-        }
-
+        callback.onSuccess();
     }
 
     public void requestStorage(PermissionsCallback callback) {
-        String[] perms = {
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE};
-
-        if (EasyPermissions.hasPermissions(fragmentActivity, perms)) {
-            callback.onSuccess();
-        } else {
-
-            EasyPermissions.requestPermissions(fragmentActivity, "请给予存储权限",
-                    1, perms);
-        }
+        callback.onSuccess();
     }
 
 
     public void requestAudio(PermissionsCallback callback) {
-        String[] perms = {
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE};
-
-        if (EasyPermissions.hasPermissions(fragmentActivity, perms)) {
-            callback.onSuccess();
-        } else {
-
-            EasyPermissions.requestPermissions(fragmentActivity, "请给予录音、存储权限",
-                    1, perms);
-        }
+        callback.onSuccess();
     }
 }
 
